107311313faae8e9b8dae62e577d0500fccd05f9
linkmm <linkmm@gmail.com>
2025-01-01 20:43:52 +0800

    update db saving function

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,8 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
+  host: process.env.PGHOST,
+  database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT || 5432,
   ssl: process.env.POSTGRES_SSL === 'true' ? {
